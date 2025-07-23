c47d6a34e9f65bc243c009e18fb1e2e081ddfbe1
Ryan Hunt <ryanahunt@outlook.com>
2025-07-22 21:40:37 -0500

    base files p2

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__, template_folder='templates',static_folder='static')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    # local dev with auto‑reload and debug info
+    app.run(host='0.0.0.0', port=8080, debug=True)
